fad628690de56f7851e799970298d440ca60c7ee
William Bispo <bispo@pop-os.localdomain>
2023-01-09 20:44:32 -0300

    Accordion almost finished

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,20 +1,41 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-  const [expendedIndex, setExpendedIndex] = useState(1);
+  const [expendedIndex, setExpendedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    if (expendedIndex === nextIndex) {
+      setExpendedIndex(-1);
+    } else {
+      setExpendedIndex(nextIndex);
+    }
+  };
 
   const renderedItems = items.map((item, index) => {
     const isExpended = index === expendedIndex;
 
+    const icon = (
+      <span className="text-2xl">
+        {isExpended ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        {isExpended && <div>{item.content}</div>}
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpended && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
