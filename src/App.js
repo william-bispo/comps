769535040097debaf16d404550f07e21b413bff4
William Bispo <bispo@pop-os.localdomain>
2023-01-16 20:53:44 -0300

    Dropdown finished

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
   return (
     <div className="flex">
       <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
     </div>
   );
 }
