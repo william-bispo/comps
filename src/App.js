bdcb0a14c0bbc071a1eaa4dfe8d810a8c68a8461
William Bispo <bispo@pop-os.localdomain>
2023-01-17 21:51:44 -0300

    Working on Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
