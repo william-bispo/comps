5915e8d52d53b990efff326f88ce9b985c3c7038
William Bispo <bispo@pop-os.localdomain>
2023-01-19 21:32:06 -0300

    Navigation finished

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,14 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from "classnames";
+import useNavigation from "../hooks/use-navigation";
 
-function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+function Link({ to, children, className, activeClassName }) {
+  const { navigate, currentPath } = useNavigation();
+
+  const classes = classNames(
+    "text-blue-500",
+    className,
+    currentPath === to && activeClassName
+  );
 
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) {
@@ -14,7 +20,7 @@ function Link({ to, children }) {
   };
 
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
