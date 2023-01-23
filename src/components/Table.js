eb27fe79c101319b09b5f2e086ba62db4de62068
William Bispo <bispo@pop-os.localdomain>
2023-01-23 20:57:50 -0300

    Working on Table. Making a reusable one.

---
+++ src/components/Table.js
@@ -0,0 +1,28 @@
+function Table({ data, config }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr className="border-b" key={fruit.name}>
+        <td className="p-3">{fruit.name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${fruit.color}`}></div>
+        </td>
+        <td className="p-3">{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
+}
+
+export default Table;
---
+++ src/pages/TablePage.js
@@ -0,0 +1,24 @@
+import Table from "../components/Table";
+
+function TablePage() {
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
+  ];
+
+  const config = [
+    { label: "Name of Fruit" },
+    { label: "Color" },
+    { label: "Score" },
+  ];
+
+  return (
+    <div>
+      <Table data={data} config={config} />
+    </div>
+  );
+}
+
+export default TablePage;
